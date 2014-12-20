a633a508cd5759748c1e455ef8d98036ccf0b516
Hyeongmin Park <hmp0077@gmail.com>
2014-12-21 00:01:10 +0900

    이제 회전까진 됨 이제 이동 해야함

--- src/Sources/Board2.java
+++ src/Sources/Board2.java
@@ -1,12 +1,16 @@
 package Sources;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.File;
-import javax.imageio.ImageIO;
 
 /**
  * Created by hyeongminpark on 14. 12. 20..
@@ -14,98 +18,313 @@ import javax.imageio.ImageIO;
 public class Board2 {
     int cardNum = 50;
 
-    ArrayList<BufferedImage> fixedTokenImages = new ArrayList<BufferedImage>();
-    ArrayList<BufferedImage> freeTokenImages = new ArrayList<BufferedImage>();
+    ArrayList<Image> freeTokenImages = new ArrayList<Image>();
+    Image[][] fixedTokenImages = new Image[5][5];
 
     Card card;
-    JPanel tokenLine;
+//    JPanel tokenLine;
 
     public void go(){
-//        showGUI();
+        doMouseSmt();
     }
 
     public Board2(){
+        for(int i = 0; i<5; i++){
+            for(int j = 0; j<5; j++){
+                fixedTokenImages[i][j] = null;
+            }
+        }
+
         String cardFileDir = "./src/Cards/Card" + cardNum + ".txt";
         card = new Card(cardFileDir);
         card.plotBoard();
-
-//        JPanel imageLine = new JPanel(new GridLayout(1, 5));
-//        imageLine.setPreferredSize(new Dimension(500, 100));
-//        JPanel imageTable = new JPanel(new GridLayout(5, 5));
-//        imageTable.setPreferredSize(new Dimension(500, 500));
-//
-//
-//        for(int i=0; i<card.getFreeTokens().size(); i++){
-//            try {
-//                if (card.getFreeTokens().get(i).getColor().equals("k")) {
-//                    freeTokenImages.add(ImageIO.read(new File("../images/TokenBlack.png")));
-//                } else if (card.getFreeTokens().get(i).getColor().equals("b")) {
-//                    freeTokenImages.add(ImageIO.read(new File("../images/TokenBlue.png")));
-//                } else if (card.getFreeTokens().get(i).getColor().equals("g")) {
-//                    freeTokenImages.add(ImageIO.read(new File("../images/TokenGreen.png")));
-//                } else if (card.getFreeTokens().get(i).getColor().equals("p")) {
-//                    freeTokenImages.add(ImageIO.read(new File("../images/TokenPurple.png")));
-//                } else if (card.getFreeTokens().get(i).getColor().equals("r")) {
-//                    freeTokenImages.add(ImageIO.read(new File("../images/TokenRed.png")));
-//                } else if (card.getFreeTokens().get(i).getColor().equals("y")) {
-//                    freeTokenImages.add(ImageIO.read(new File("../images/TokenYellow.png")));
-//                } else {
-//                    System.out.println("invalid color key inputted: " + card.getFreeTokens().get(i).getColor());
-//                }
-//                imageLine.add(new JLabel(new ImageIcon(freeTokenImages.get(freeTokenImages.size() - 1))));
-//            }catch(IOException ex){
-//                ex.printStackTrace();
-//            }
-//        }
-//
-////        for(int i=card.getFreeTokens().size(); i<5; i++){
-////            freeTokenImages.add(null);
-////            imageLine.add(null);
-////        }
-//
-//        for(int i=0; i<5; i++){
-//            for(int j=0; j<5; j++){
-//                if(card.getTokenTable()[i][j] != null){
-//                    try {
-//                        if (card.getTokenTable()[i][j].getColor().equals("k")) {
-//                            fixedTokenImages.add(ImageIO.read(new File("../images/TokenBlack.png")));
-//                        } else if (card.getTokenTable()[i][j].getColor().equals("b")) {
-//                            fixedTokenImages.add(new DTPicture(createImageIcon("../images/TokenBlue.png", "TokenBlue.png").getImage(), card.getTokenTable()[i][j]));
-//                        } else if (card.getTokenTable()[i][j].getColor().equals("g")) {
-//                            fixedTokenImages.add(new DTPicture(createImageIcon("../images/TokenGreen.png", "TokenGreen.png").getImage(), card.getTokenTable()[i][j]));
-//                        } else if (card.getTokenTable()[i][j].getColor().equals("p")) {
-//                            fixedTokenImages.add(new DTPicture(createImageIcon("../images/TokenPurple.png", "TokenPurple.png").getImage(), card.getTokenTable()[i][j]));
-//                        } else if (card.getTokenTable()[i][j].getColor().equals("r")) {
-//                            fixedTokenImages.add(new DTPicture(createImageIcon("../images/TokenRed.png", "TokenRed.png").getImage(), card.getTokenTable()[i][j]));
-//                        } else if (card.getTokenTable()[i][j].getColor().equals("y")) {
-//                            fixedTokenImages.add(new DTPicture(createImageIcon("../images/TokenYellow.png", "TokenYellow.png").getImage(), card.getTokenTable()[i][j]));
-//                        } else {
-//                            System.out.println("invalid color key inputted: " + card.getTokenTable()[i][j].getColor());
-//                        }
-////            fixedTokenImages.add(new DTPicture(createImageIcon("../images/" + tokenNameList[i], tokenNameList[i]).getImage()));
-//                        fixedTokenImages.get(fixedTokenImages.size() - 1).setTransferHandler(picHandler);
-//                        imageTable.add(fixedTokenImages.get(fixedTokenImages.size() - 1));
-//                    }catch(IOException ex){
-//                        ex.printStackTrace();
-//                    }
-//                }else{
-//                        fixedTokenImages.add(new DTPicture(null, null));
-//                        fixedTokenImages.get(fixedTokenImages.size() - 1).setTransferHandler(picHandler);
-//                        imageTable.add(fixedTokenImages.get(fixedTokenImages.size() - 1));
-//
-//                }
-//            }
-//        }
     }
 
-    protected static ImageIcon createImageIcon(String path, String description) {
-        java.net.URL imageURL = Board.class.getResource(path);
+    public void doMouseSmt(){
+//        tokenUpdate();
+        showGUI();
+    }
 
-        if (imageURL == null) {
-            System.err.println("Resource not found: " + path);
-            return null;
-        } else {
-            return new ImageIcon(imageURL, description);
+    public void showGUI(){
+        JFrame mainFrame = new JFrame("Game Board");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        TokensImageDraw draw = new TokensImageDraw(card);
+
+        mainFrame.getContentPane().add(draw);
+
+        mainFrame.pack();
+        mainFrame.setSize(500, 620); // 위쪽 바가 20픽셀 차지한다.
+        mainFrame.setVisible(true);
+//        mainFrame.repaint();
+    }
+
+    class TokensImageDraw extends JPanel implements MouseListener, MouseMotionListener{
+        private Card card;
+
+        int sX = -1, sY = -1;
+        boolean dragging = false;
+        int curX = -1, curY = -1;
+
+        //이 클래스의 생성자이다.
+        public TokensImageDraw(Card card){
+            System.out.println("JPanel 에 카드 객체 받아옴");
+            addMouseListener(this);
+            addMouseMotionListener(this);
+            this.card = card;
+        }
+
+        public void controlVhange(MouseEvent e){
+            repaint();
+        }
+
+        public Token guessToken(int x, int y){
+            int tokenPosX = x/100;
+            int tokenPosY = y/100; // freetoken라인을 고려해서 6줄로 친다
+
+            System.out.println("guessed token pos x: "+ tokenPosX);
+            System.out.println("guessed token pos y: "+ tokenPosY);
+
+            if(tokenPosY==0){
+                System.out.println("guessing from free token line");
+                return card.getFreeTokenTable()[tokenPosX];
+            }else{
+                System.out.println("guessing from token table");
+                return card.getTokenTable()[tokenPosY-1][tokenPosX];
+            }
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent event) {
+            if(event.getClickCount()==2){
+                Point point = event.getPoint();
+                int dX = point.x;
+                int dY = point.y;
+
+                Token token = guessToken(dX, dY);
+
+                if(dY==0){
+                    return;
+                }
+
+                if(token.getRotateAble()){
+                    token.rotate();
+//                    this.image = rotate(this.image);
+//                    repaint();
+                }else{
+                    System.out.println("this token set unrotatable");
+                }
+
+                event.consume();
+            }
+            repaint();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent event) {}
+
+        @Override
+        public void mouseExited(MouseEvent event) {}
+
+        @Override
+        public void mousePressed(MouseEvent event) {
+
+            Point point = event.getPoint();
+
+            sX = point.x;
+
+            sY = point.y;
+
+            dragging = true;
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent event) {
+            dragging = false;
+
+            Point p = event.getPoint();
+
+            System.out.println("MOUSE MOVE IS " + sX + "," + sY + " to " + p.x + "," + p.y);
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent event) {
+
+            Point p = event.getPoint();
+
+            // System.err.println("mouse drag to " + p);
+
+//        showStatus("mouse Dragged to " + p);
+
+            curX = p.x;
+
+            curY = p.y;
+
+            Token token = guessToken(sX, sY);
+            if(token.getMoveAble()){
+                token.setPosX(p.x/100);
+                token.setPosY(p.y/100);
+
+            }
+            repaint();
+
+            if (dragging) {
+
+                repaint();
+
+            }
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {}
+
+        public void paintComponent(Graphics g){
+            System.out.println("paintComponent 들어옴");
+//            Image image2 = new ImageIcon("../images/TokenBlack.png").getImage();
+//            g.drawImage(image2, 0, 0, this);
+
+            //free토큰을 우선 한줄로 그림
+            for(int i = 0; i < card.getFreeTokens().size(); i++){
+                System.out.println("자유토큰 그리기시작.. "+ i);
+
+                Token token = card.getFreeTokens().get(i);
+                System.out.println(token.getColor() + " 그릴 차례 ");
+                try {
+                    if (token.getColor().equals("k")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenBlack.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, i * 100, 0, this);
+
+                    } else if (token.getColor().equals("b")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenBlue.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, i * 100, 0, this);
+
+                    } else if (token.getColor().equals("g")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenGreen.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, i * 100, 0, this);
+
+                    } else if (token.getColor().equals("p")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenPurple.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, i * 100, 0, this);
+
+                    } else if (token.getColor().equals("r")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenRed.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, i * 100, 0, this);
+
+                    } else if (token.getColor().equals("y")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenYellow.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, i * 100, 0, this);
+                    }
+                }catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+
+            //fixture 토큰을 격자로 그림
+            for(int i = 0; i < card.getFixtureTokens().size(); i++){
+                System.out.println("고정토큰 그리기시작.. "+ i);
+
+                Token token = card.getFixtureTokens().get(i);
+                System.out.println(token.getColor() + " 그릴 차례 ");
+
+                try {
+                    if (token.getColor().equals("k")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenBlack.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, token.getPosX() * 100, (token.getPosY() + 1) * 100, this);
+
+                    } else if (token.getColor().equals("b")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenBlue.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, token.getPosX() * 100, (token.getPosY() + 1) * 100, this);
+
+                    } else if (token.getColor().equals("g")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenGreen.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, token.getPosX() * 100, (token.getPosY() + 1) * 100, this);
+
+                    } else if (token.getColor().equals("p")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenPurple.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, token.getPosX() * 100, (token.getPosY() + 1) * 100, this);
+
+                    } else if (token.getColor().equals("r")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenRed.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, token.getPosX() * 100, (token.getPosY() + 1) * 100, this);
+
+                    } else if (token.getColor().equals("y")) {
+                        System.out.println(token.getColor() + " 그리는중");
+                        Image image = ImageIO.read(this.getClass().getResource("../images/TokenYellow.png"));
+                        image = setTokenImageDir(image, token);
+                        g.drawImage(image, token.getPosX() * 100, (token.getPosY() + 1) * 100, this);
+                    }
+                }catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        public Image setTokenImageDir(Image image, Token token){
+            if(token.getDir()==12){
+                return image;
+            }else if(token.getDir()==3){
+                return rotate(image);
+            }else if(token.getDir()==6){
+                return rotate(rotate(image));
+            }else if(token.getDir()==9){
+                return rotate(rotate(rotate(image)));
+            }else{
+                System.out.println("token.getDir 에서 이상한 값 받음;; : " + token.getDir());
+                return image;
+            }
+        }
+
+        public Image rotate(Image img)
+        {
+            BufferedImage oldImage = imageToBufImage(img);
+
+            AffineTransform affineTransform = new AffineTransform();
+            affineTransform.rotate(Math.toRadians(90), oldImage.getWidth() / 2, oldImage.getHeight() / 2);
+
+            AffineTransformOp opRotated = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR);
+            BufferedImage newImage = opRotated.filter(oldImage, null);
+
+            return bufImageToImage(newImage);
+        }
+
+        public BufferedImage imageToBufImage(Image img){
+            if(img instanceof BufferedImage){
+                return (BufferedImage)img;
+            }
+            BufferedImage bufferedImage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+            Graphics2D tmp = bufferedImage.createGraphics();
+            tmp.drawImage(img, 0, 0, null);
+            tmp.dispose();
+
+            return bufferedImage;
+        }
+
+        public Image bufImageToImage(BufferedImage buffImg){
+            return Toolkit.getDefaultToolkit().createImage(buffImg.getSource());
         }
     }
 }
